1788d7978bee3977dc37a1f350f6f77529075e82
masterymastery <masterymastery@sina.com>
2019-10-31 18:14:10 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,284 +1,366 @@
 <template>
-  <div id="app">
-    <div id="cycontainner"></div>123
-  </div>
+    <div id="app">
+        <div id="cycontainner"></div>
+        123
+    </div>
 </template>
 <script>
 import {
-  getGraphData,
-  getRelationshipsAll
+    getGraphData,
+    getRelationshipsAll
 } from "@/service/search-graph-data.js";
 import cytoscape from "cytoscape";
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import popper from "cytoscape-popper";
+
 export default {
-  name: "app",
-  data() {
-    return {
-      dataSource: []
-    };
-  },
-  mounted() {
-    var self = this;
-    var cy = cytoscape({
-      container: document.getElementById("cycontainner")
-      /* elements: [
-                // flat array of nodes and edges
+    name: "app",
+    data() {
+        return {
+            dataSource: []
+        };
+    },
+    mounted() {
+        //cytoscape.use(dblclick);
+        var self = this;
+        var cy = cytoscape({
+            container: document.getElementById("cycontainner"),
+            style: [
                 {
-                    // node n1
-                    group: "nodes",
-                    data: {
-                        // element data (put json serialisable dev data here)
-                        id: "n1", // mandatory (string) id for each element, assigned automatically on undefined
-                        parent: "nparent" // indicates the compound node parent id; not defined => no parent
-                        // (`parent` can be effectively changed by `eles.move()`)
-                    },
-                    position: {
-                        // the model position of the node (optional on init, mandatory after)
-                        x: 100,
-                        y: 100
+                    selector: "node",
+                    style: {
+                        label: "data(properties.name)",
+                        "font-size": 14,
+                        color: "black",
+                        "text-valign": "center"
                     }
                 },
                 {
-                    // node n2
-                    group: "nodes",
-                    data: { id: "n2" },
-                    renderedPosition: { x: 200, y: 200 } // can alternatively specify position in rendered on-screen pixels
+                    selector: "edge",
+                    style: {
+                        label: "data(label)",
+                        "font-size": 12,
+                        color: "gray",
+                        "line-color": "#ffaaaa"
+                    }
                 },
                 {
-                    // edge e1
-                    data: {
-                        id: "e1",
-                        source: "n1", // the source node id (edge comes from this node)
-                        target: "n2" // the target node id (edge goes to this node)
+                    selector: "edge.bezier",
+                    style: {
+                        "curve-style": "bezier",
+                        "control-point-step-size": 40,
+                        "target-arrow-color": "#ccc",
+                        "target-arrow-shape": "triangle"
+                    }
+                },
+                {
+                    selector: "edge.unbundled-bezier",
+                    style: {
+                        "curve-style": "unbundled-bezier",
+                        "target-arrow-color": "#ccc",
+                        "target-arrow-shape": "triangle",
+                        "control-point-distances": 10,
+                        "control-point-weights": 0.1
+                    }
+                },
+                {
+                    selector: "[label = 'device']",
+                    style: {
+                        "background-color": "blue"
+                    }
+                },
+                {
+                    selector: "[label = 'interface']",
+                    style: {
+                        "background-color": "pink",
+                        width: "20",
+                        height: 20
                     }
                 }
-            ] */
-    });
-    /* cy.add({
-            group: "nodes",
-            data: { weight: 75 },
-            position: { x: 200, y: 200 }
+            ]
         });
-        var eles = cy.add([
-            {
-                group: "nodes",
-                data: { id: "n0" },
-                position: { x: 100, y: 100 }
-            },
-            {
-                group: "nodes",
-                data: { id: "n1" },
-                position: { x: 200, y: 200 }
-            },
-            { group: "edges", data: { id: "e0", source: "n0", target: "n1" } }
-        ]); */
-
-    var nodes = [];
-    var edges = [];
-    var elements = [];
-    getGraphData({ script: "MATCH (n) RETURN n LIMIT 1" }).then(function(
-      res,
-      rec
-    ) {
-      // or relationships
-      elements = self.filterDataModel(
-        res.result.graph.data.nodes,
-        res.result.graph.data.relationships
-      );
-      cy.add(elements);
-      self.autoLayout(cy);
-      var layout = cy.layout({ name: "cose" });
-      layout.run();
-      cy.nodes().on("click", self.nodeClick.bind(self, cy));
-    });
-    console.log(cy);
-  },
-  methods: {
-    nodeClick(cy, e) {
-      var self = this;
-      var ele = e.target;
-      getRelationshipsAll(ele.data()).then(function(res, rec) {
-        // console.log(_elements);
-
-        var nodes = self.filterNodeModel(res.result.graph.data.nodes);
-        var relationships = self.filterEdgeModel(
-          res.result.graph.data.relationships
-        );
-        nodes = self.duplicateNodeRemoval(nodes, cy);
-        relationships = self.duplicateEdgeRemoval(relationships, cy);
-        console.log(nodes);
-        console.log(relationships);
-        var _elements;
-        _elements = nodes.concat(relationships);
-        cy.add(_elements);
-
-        cy.nodes().on("click", self.nodeClick.bind(self, cy));
-
-        self.autoLayout(cy);
-        var layout = cy.layout({ name: "cose" });
-        layout.run();
-      });
+        var nodes = [];
+        var edges = [];
+        var elements = [];
+        getGraphData({ script: "MATCH (n) RETURN n LIMIT 1" }).then(function(
+            res,
+            rec
+        ) {
+            elements = self.dealDataModel(
+                res.result.graph.data.nodes,
+                res.result.graph.data.relationships
+            );
+            cy.add(elements);
+            self.autoLayout(cy);
+            var layout = cy.layout({ name: "cose" });
+            layout.run();
+            cy.dblclick();
+            cy.nodes().on("dblclick", self.nodeClick.bind(self, cy));
+            cy.nodes().on("mouseover", self.mouseover.bind(self, cy));
+            cy.nodes().on("mouseout", self.mouseout.bind(self, cy));
+        });
+        console.log(cy);
     },
-    filterDataModel(nodes, edges) {
-      var self = this;
-      var _nodes = [];
-      nodes.forEach(function(item) {
-        _nodes.push({
-          group: "nodes",
-          data: {
-            id: item.vid,
-            vid: item.vid,
-            label: item.label,
-            properties: item.properties
-          },
-          position: { x: 0, y: 0 }
-        });
-      });
-      var _edges = [];
-      edges.forEach(function(item, index) {
-        _edges.push({
-          group: "edges",
-          data: {
-            id: item.source + "-" + item.destination,
-            label: item.label,
-            source: item.source,
-            target: item.destination,
-            properties: item.properties
-          },
-          position: { x: 0, y: 0 }
-        });
-      });
-      return _nodes.concat(_edges);
-    },
-    filterNodeModel(nodes) {
-      var self = this;
-      var _nodes = [];
-      nodes.forEach(function(item) {
-        _nodes.push({
-          group: "nodes",
-          data: {
-            id: item.vid,
-            vid: item.vid,
-            label: item.label,
-            properties: item.properties
-          },
-          position: { x: 0, y: 0 }
-        });
-      });
-      return _nodes;
-    },
-    filterEdgeModel(edges) {
-      var _edges = [];
-      edges.forEach(function(item, index) {
-        _edges.push({
-          group: "edges",
-          data: {
-            id: item.source + "-" + item.destination,
-            label: item.label,
-            source: item.source,
-            target: item.destination,
-            properties: item.properties
-          },
-          position: { x: 0, y: 0 }
-        });
-      });
-      return _edges;
-    },
-    duplicateNodeRemoval(nodes, cy) {
-      var self = this;
-      /* var _nodes = cy.$(nodes);
-      return cy
-        .nodes()
-        .difference(_nodes)
-        .data(); */
-      var _nodes = [];
-      nodes.forEach(function(item) {
-        for (var i = 0; i < cy.nodes().length; i++) {
-          /* item拿出来和画布中的每一个点比，如果每一个点都不同才push出来 */
-          if (item.data.vid === cy.nodes()[i].data().vid) {
-            break;
-          }
-          if (
-            i === cy.nodes().length - 1 &&
-            item.data.vid !== cy.nodes()[i].data().vid
-          ) {
-            _nodes.push(item);
-          }
+    methods: {
+        nodeClick(cy, e) {
+            var self = this;
+            var ele = e.target;
+            getRelationshipsAll(ele.data()).then(function(res, rec) {
+                // console.log(_elements);
+                var nodes = self.dealNodeModel(res.result.graph.data.nodes);
+                var relationships = self.dealEdgeModel(
+                    res.result.graph.data.relationships
+                );
+                nodes = self.duplicateNodeRemoval(nodes, cy);
+                relationships = self.duplicateEdgeRemoval(relationships, cy);
+                console.log(nodes);
+                console.log(relationships);
+                var _elements;
+                _elements = nodes.concat(relationships);
+                cy.add(_elements);
+                /* 事件不可重复绑定，应该只给新增的node绑定事件 */
+                // cy.nodes().on("click", self.nodeClick.bind(self, cy));
+                // cy.on("click", '_elements', self.nodeClick.bind(self, cy));
+                self.autoLayout(cy);
+                var layout = cy.layout({ name: "cose" });
+                layout.run();
+            });
+        },
+        mouseover(cy, e) {
+            var self = this;
+            var a = e.target;
+            var popperA = a.popper({
+                content: function() {
+                    return self.makeDiv(a.data().vid, "Sticky position div");
+                }
+            });
+            popperA.scheduleUpdate();
+            /*    var tippyA = self.makeTippy(a, "foo");
+            tippyA.show(); */
+            //console.log(e.target);
+        },
+        mouseout(cy, e) {
+            var self = this;
+            var a = e.target;
+            var tooltip = document.getElementById(a.data().vid);
+            if (tooltip) {
+                tooltip.remove();
+            } else {
+                console.log("没找到要移除的tooltip");
+            }
+        },
+        makeDiv(id, text) {
+            var div = document.createElement("div");
+            div.id = id;
+            div.classList.add("popper-div");
+            div.innerHTML = text;
+            document.body.appendChild(div);
+            return div;
+        },
+        makeTippy(node, text) {
+            return tippy(node.popperRef(), {
+                content: function() {
+                    var div = document.createElement("div");
+
+                    div.innerHTML = text;
+
+                    return div;
+                },
+                trigger: "manual",
+                arrow: true,
+                placement: "bottom",
+                hideOnClick: false,
+                multiple: true,
+                sticky: true
+            });
+        },
+        dealDataModel(nodes, edges) {
+            var self = this;
+            var _nodes = [];
+            nodes.forEach(function(item) {
+                _nodes.push({
+                    group: "nodes",
+                    data: {
+                        id: item.vid,
+                        vid: item.vid,
+                        label: item.label,
+                        properties: item.properties
+                    },
+                    position: { x: 0, y: 0 }
+                });
+            });
+            var _edges = [];
+            edges.forEach(function(item, index) {
+                _edges.push({
+                    group: "edges",
+                    data: {
+                        id: item.source + "-" + item.destination,
+                        label: item.label,
+                        source: item.source,
+                        target: item.destination,
+                        properties: item.properties
+                    },
+                    position: { x: 0, y: 0 }
+                });
+            });
+            return _nodes.concat(_edges);
+        },
+        dealNodeModel(nodes) {
+            var self = this;
+            var _nodes = [];
+            var parent = nodes.find(function(item) {
+                return item.label === "device";
+            });
+            nodes.forEach(function(item) {
+                if (item.label === "interface") {
+                    _nodes.push({
+                        group: "nodes",
+                        data: {
+                            id: item.vid,
+                            vid: item.vid,
+                            label: item.label,
+                            properties: item.properties
+                            //parent: parent.vid
+                        },
+                        position: { x: 0, y: 0 }
+                    });
+                } else {
+                    _nodes.push({
+                        group: "nodes",
+                        data: {
+                            id: item.vid,
+                            vid: item.vid,
+                            label: item.label,
+                            properties: item.properties
+                        },
+                        position: { x: 0, y: 0 }
+                    });
+                }
+            });
+            return _nodes;
+        },
+        dealEdgeModel(edges) {
+            var _edges = [];
+            edges.forEach(function(item, index) {
+                _edges.push({
+                    group: "edges",
+                    data: {
+                        id: item.source + "-" + item.destination,
+                        label: item.label,
+                        source: item.source,
+                        target: item.destination,
+                        properties: item.properties
+                    },
+                    position: { x: 0, y: 0 },
+                    classes: "unbundled-bezier"
+                });
+            });
+            return _edges;
+        },
+        duplicateNodeRemoval(nodes, cy) {
+            var self = this;
+            /* var _nodes = cy.$(nodes);
+              return cy
+                .nodes()
+                .difference(_nodes)
+                .data(); */
+            var _nodes = [];
+            nodes.forEach(function(item) {
+                for (var i = 0; i < cy.nodes().length; i++) {
+                    /* item拿出来和画布中的每一个点比，如果每一个点都不同才push出来 */
+                    if (item.data.vid === cy.nodes()[i].data().vid) {
+                        break;
+                    }
+                    if (
+                        i === cy.nodes().length - 1 &&
+                        item.data.vid !== cy.nodes()[i].data().vid
+                    ) {
+                        _nodes.push(item);
+                        cy.on(
+                            "dblclick",
+                            "[vid = " + item.data.vid + "]",
+                            self.nodeClick.bind(self, cy)
+                        );
+                    }
+                }
+            });
+
+            return _nodes;
+        },
+        duplicateEdgeRemoval(edges, cy) {
+            var self = this;
+            /*  var _edges = cy.$(edges);
+              return cy
+                .edges()
+                .difference(_edges)
+                .data(); */
+            var _edges = [];
+            if (cy.edges().length > 0) {
+                edges.forEach(function(item, index) {
+                    for (var i = 0; i < cy.edges().length; i++) {
+                        /* item拿出来和画布中的每一个点比，如果每一个点都不同才push出来 */
+                        if (item.data.id === cy.edges()[i].data().id) {
+                            break;
+                        }
+                        if (
+                            i === cy.edges().length - 1 &&
+                            item.data.id !== cy.edges()[i].data().id
+                        ) {
+                            _edges.push(item);
+                        }
+                    }
+                });
+                return _edges;
+            } else {
+                return edges;
+            }
+        },
+        autoLayout(cy) {
+            var layout = cy.layout({ name: "cose" });
+            layout.run();
         }
-      });
-      return _nodes;
-    },
-    duplicateEdgeRemoval(edges, cy) {
-      var self = this;
-      /*  var _edges = cy.$(edges);
-      return cy
-        .edges()
-        .difference(_edges)
-        .data(); */
-      var _edges = [];
-      if (cy.edges().length > 0) {
-        edges.forEach(function(item, index) {
-          for (var i = 0; i < cy.edges().length; i++) {
-            /* item拿出来和画布中的每一个点比，如果每一个点都不同才push出来 */
-            if (item.data.id === cy.edges()[i].data().id) {
-              break;
-            }
-            if (
-              i === cy.edges().length - 1 &&
-              item.data.id !== cy.edges()[i].data().id
-            ) {
-              _edges.push(item);
-            }
-          }
-          /* if (
-            !Array(cy.edges().data()).find(function(_item) {
-              return item.data.id === _item.id;
-            })
-          ) {
-            _edges.push(item);
-          } */
-        });
-        return _edges;
-      } else {
-        return edges;
-      }
-    },
-    autoLayout(cy) {
-      var layout = cy.layout({ name: "cose" });
-      layout.run();
     }
-  }
 };
 </script>
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+.popper-div {
+    /* just an example */
+    border: 1px solid red;
+    background: #fff;
+    z-index: 9999;
+    padding: 0.25em;
+    pointer-events: none;
 }
 #cycontainner {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: 999;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 999;
 }
 #cycontainner canvas {
-  left: 0;
-  top: 0;
+    left: 0;
+    top: 0;
 }
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import cytoscape from "cytoscape";
+import dblclick from "cytoscape-dblclick";
+import popper from "cytoscape-popper";
 
-Vue.config.productionTip = false
+cytoscape.use(popper);
+cytoscape.use(dblclick);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount("#app");
